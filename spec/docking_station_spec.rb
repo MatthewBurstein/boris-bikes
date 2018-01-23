42e971373a964584cb93373ad7351efea4156d57
Serena Hathi <hathi.serena@gmail.com>
2018-01-23 15:35:04 +0000

    Add ability to release bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,16 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+
+  describe "#release_bike" do
+    it 'gets a bike and expects the bike to be working' do
+    station = DockingStation.new
+    expect(station.release_bike).to be_working
+    end
+  end   
 end
+
+
+
+
+# alter docking station class so release_bike returns a new instance of the bike class
